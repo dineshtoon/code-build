cbd90fec49672eaff7adc2d7abecc585aae27e74
Dinesh <dinesh@gmail.com>
2021-08-11 06:05:39 +0530

    adding soure

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,27 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    public long add(long first, long second) {
+        int a;
+        String str;
+        return first + second;
+    }
+
+    public long subtract(long first, long second) {
+        return first - second;
+    }
+
+    public long multiply(long first, long second) {
+        return first * second;
+    }
+
+    public long divide(long first, long second) {
+        return first / second;
+    }
+
+}
